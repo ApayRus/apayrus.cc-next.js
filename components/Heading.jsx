b37045177096821d45bd9c285b73cfc6881dc16c
apayrus <rustamapaev@gmail.com>
2022-01-30 08:48:09 +0300

    rewrite Heading

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,16 +1,25 @@
-import Logo from './Logo'
-import Title from './Title'
+import Link from 'next/link'
+import theme from '../styles/theme'
 import LangSwitcher from './LangSwitcher'
 
 const Heading = props => {
 	const { title, tags } = props
 	return (
-		<header style={styles.root}>
-			<div style={styles.logo}>
-				<Logo />
-			</div>
-			<div style={styles.title}>
-				<Title title={title} tags={tags} />
+		<header style={styles.heading}>
+			<div style={styles.titleLogoContainer}>
+				<Link href='/'>
+					<img style={styles.logo} src='/logo.png' alt='apayrus.cc logo' />
+				</Link>
+				<div style={styles.titleContainer}>
+					<h1 style={styles.title}>{title}</h1>
+					<div style={styles.tags}>
+						{tags.map(tag => (
+							<div style={styles.tag} key={tag}>
+								{tag}
+							</div>
+						))}
+					</div>
+				</div>
 			</div>
 			<div style={styles.langSwitcher}>
 				<LangSwitcher />
@@ -20,18 +29,24 @@ const Heading = props => {
 }
 
 const styles = {
-	root: {
-		display: 'flex'
+	heading: { padding: '5px' },
+	titleLogoContainer: {
+		display: 'flex',
+		alignItems: 'center'
 	},
-	logo: {
-		flex: 1
+	titleContainer: {
+		display: 'inline-block',
+		marginLeft: '10px'
 	},
-	title: {
-		flex: 3
-	},
+	title: { margin: 0, color: theme.color1 },
+	logo: { width: '35%', maxWidth: '70px' },
 	langSwitcher: {
-		flex: 1
-	}
+		position: 'absolute',
+		top: 10,
+		right: 10
+	},
+	tags: { marginLeft: '4px', color: 'grey' },
+	tag: { display: 'inline-block' }
 }
 
 export default Heading
--- components/LangSwitcher.jsx
+++ components/LangSwitcher.jsx
@@ -1,24 +1,56 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import theme from '../styles/theme'
 
 const LangSwitcher = () => {
 	const router = useRouter()
 	const {
 		pathname,
-		query: { slug }
+		query: { slug },
+		locales,
+		locale: activeLocale
 	} = router
+
+	const isActiveLocale = locale => {
+		return locale === activeLocale
+	}
 
 	const path = slug || pathname // slug only on dynamic routing like: pages/projects/[slug].jsx
 	return (
-		<>
-			<Link href={path} locale='en'>
-				<a>en</a>
-			</Link>
-			<Link href={path} locale='ru'>
-				<a>ru</a>
-			</Link>
-		</>
+		<div style={styles.langSwitcherContainer}>
+			{locales.map(locale => {
+				return (
+					<Link key={locale} href={path} locale={locale}>
+						<div
+							style={
+								isActiveLocale(locale)
+									? { ...styles.locale, ...styles.activeLocale }
+									: styles.locale
+							}
+						>
+							{locale}
+						</div>
+					</Link>
+				)
+			})}
+		</div>
 	)
+}
+
+const styles = {
+	langSwitcherContainer: {
+		// border: '1px solid red'
+	},
+	locale: {
+		padding: '2px',
+		cursor: 'pointer',
+		borderRadius: '8px',
+		fontSize: '0.7rem'
+	},
+	activeLocale: {
+		backgroundColor: theme.color1,
+		color: 'white'
+	}
 }
 
 export default LangSwitcher

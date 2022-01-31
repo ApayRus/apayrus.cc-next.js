f9262a1791e1209346034a05d6145e589a3e8f1a
apayrus <rustamapaev@gmail.com>
2022-01-31 12:26:31 +0300

    add video to About

--- components/About.jsx
+++ components/About.jsx
@@ -1,14 +1,32 @@
+import styles from './About.module.css'
+
 const About = props => {
-	const { title, description } = props
+	const { title, description, videos } = props
+	const [firstVideo] = videos
+
 	return (
-		<div>
-			<h1 dangerouslySetInnerHTML={{ __html: title }} />
-			<div
-				dangerouslySetInnerHTML={{
-					__html: description
-				}}
-			></div>
-		</div>
+		<section className={styles.root}>
+			<h2 className={styles.title}>{title}</h2>
+			<div className={styles.content}>
+				<div className={styles.video}>
+					<iframe
+						width='160'
+						height='160'
+						src={firstVideo.href}
+						// title='YouTube video player'
+						frameBorder='0'
+						allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+						allowFullScreen
+					></iframe>
+				</div>
+				<div
+					className={styles.text}
+					dangerouslySetInnerHTML={{
+						__html: description
+					}}
+				></div>
+			</div>
+		</section>
 	)
 }
 

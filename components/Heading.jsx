c9f0d97bbdd474e9bdbd0808c15c89c82f64da63
apayrus <rustamapaev@gmail.com>
2022-01-31 13:00:41 +0300

    restyle Heading

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,52 +1,31 @@
 import Link from 'next/link'
-import theme from '../styles/theme'
 import LangSwitcher from './LangSwitcher'
+import styles from './Heading.module.css'
 
 const Heading = props => {
 	const { title, tags } = props
 	return (
-		<header style={styles.heading}>
-			<div style={styles.titleLogoContainer}>
+		<header className={styles.heading}>
+			<div className={styles.titleLogoContainer}>
 				<Link href='/'>
-					<img style={styles.logo} src='/logo.png' alt='apayrus.cc logo' />
+					<img className={styles.logo} src='/logo.png' alt='apayrus.cc logo' />
 				</Link>
-				<div style={styles.titleContainer}>
-					<h1 style={styles.title}>{title}</h1>
-					<div style={styles.tags}>
+				<div className={styles.titleContainer}>
+					<h1 className={styles.title}>{title}</h1>
+					<div className={styles.tags}>
 						{tags.map(tag => (
-							<div style={styles.tag} key={tag}>
+							<div className={styles.tag} key={tag}>
 								{tag}
 							</div>
 						))}
 					</div>
 				</div>
 			</div>
-			<div style={styles.langSwitcher}>
+			<div className={styles.langSwitcher}>
 				<LangSwitcher />
 			</div>
 		</header>
 	)
-}
-
-const styles = {
-	heading: { padding: '5px' },
-	titleLogoContainer: {
-		display: 'flex',
-		alignItems: 'center'
-	},
-	titleContainer: {
-		display: 'inline-block',
-		marginLeft: '10px'
-	},
-	title: { margin: 0, color: theme.color1 },
-	logo: { width: '35%', maxWidth: '70px', cursor: 'pointer' },
-	langSwitcher: {
-		position: 'absolute',
-		top: 10,
-		right: 10
-	},
-	tags: { marginLeft: '4px', color: 'grey' },
-	tag: { display: 'inline-block' }
 }
 
 export default Heading

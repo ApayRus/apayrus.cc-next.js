e899270282db0eebd95c18bb9a6ef1127988be21
apayrus <rustamapaev@gmail.com>
2022-01-31 12:44:37 +0300

    add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,19 @@
+import styles from './Footer.module.css'
+
 const Footer = props => {
 	const { socialMedia } = props
+	const { links } = socialMedia
+
 	return (
-		<footer>
-			<pre>{JSON.stringify(socialMedia, null, 2)}</pre>
+		<footer className={styles.root}>
+			{links.map(elem => {
+				const { href, text } = elem
+				return (
+					<div key={text} className={styles.link}>
+						<a href={href}>{text}</a>
+					</div>
+				)
+			})}
 		</footer>
 	)
 }

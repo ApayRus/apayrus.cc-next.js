d69aca39de202b64968f465686986054cdf3b2d8
apayrus <rustamapaev@gmail.com>
2022-02-09 14:24:46 +0300

    add text captions to images block

--- components/project/ImagesSlider.jsx
+++ components/project/ImagesSlider.jsx
@@ -5,10 +5,19 @@ const ImagesSlider = props => {
 
 	return (
 		<div className={styles.root}>
-			<div className={styles.box}>
+			<div className={styles.imagesContainer}>
 				{images.map(image => {
 					const { text, href } = image
-					return <img key={text} className={styles.image} src={href} alt={text} />
+					return (
+						<div
+							className={styles.imageContainer}
+							style={{ display: 'flex', flexDirection: 'column' }}
+							key={text}
+						>
+							<img className={styles.image} src={href} alt={text} />
+							<div className={styles.imageCaption}>{text}</div>
+						</div>
+					)
 				})}
 			</div>
 		</div>
